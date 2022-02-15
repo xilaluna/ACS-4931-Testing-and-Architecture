6bb0cab07f446158757671b79fc0dcb20253bd7a
xiluna <xilaluna2@gmail.com>
2022-02-15 10:32:33 -0800

    [upd] excerise 11

--- exercises/simplify-conditionals/gauge_eyeballs.py
+++ exercises/simplify-conditionals/gauge_eyeballs.py
@@ -13,8 +13,10 @@ eye_width = 24.2   # [mm]
 eye_height = 23.7  # [mm]
 
 iris_width = 20.2  # [mm]
-iris_height = 19.7 # [mm]
+iris_height = 19.7  # [mm]
 
-if eye_size > 0.45 and (math.pi*iris_width/2*iris_height/2) / eye_size >= 0.69 and \
-        eye_height/eye_width >= 0.59:
+check_eye = eye_size > 0.45 and (math.pi*iris_width/2*iris_height/2) / eye_size >= 0.69 and \
+    eye_height/eye_width >= 0.59
+
+if check_eye:
     print("I’m sorry I wasn’t part of your past, can I make it up by being in your future?")
