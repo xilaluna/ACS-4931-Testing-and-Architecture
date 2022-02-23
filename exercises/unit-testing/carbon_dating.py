1f960560e9e1c7e835d7df479f8c8d970c84b554
xiluna <xilaluna2@gmail.com>
2022-02-22 19:16:47 -0800

    excercise 1

--- exercises/unit-testing/carbon_dating.py
+++ exercises/unit-testing/carbon_dating.py
@@ -5,9 +5,10 @@ DECAY_CONSTANT = -0.693
 
 # TODO: Write a unit test which feed 0.35 to the function.
 # The result should be '8680.34'.
-    # - Does the function handle every possible input correctly?
-    # - What if the input is zero or negative?
-    # - Add the necessary logic to make sure the function handle every possible input properly. Then write a unit test against this special case.
+# - Does the function handle every possible input correctly?
+# - What if the input is zero or negative?
+# - Add the necessary logic to make sure the function handle every possible input properly. Then write a unit test against this special case.
+
 
 def get_age_carbon_14_dating(carbon_14_ratio):
     """Returns the estimated age of the sample in year.
@@ -15,4 +16,8 @@ def get_age_carbon_14_dating(carbon_14_ratio):
     in the sample conpared to the amount in living
     tissue (unitless).
     """
+    if carbon_14_ratio >= 0:
+        raise TypeError(
+            "number is not supported by the mathematical operation")
+
     return math.log(carbon_14_ratio) / DECAY_CONSTANT * T_HALF
--- exercises/unit-testing/tests/test_carbon_dating.py
+++ exercises/unit-testing/tests/test_carbon_dating.py
@@ -1,7 +1,6 @@
-import unittest
 import pytest
 import math
-from ..carbon_dating import get_age_carbon_14_dating
+from carbon_dating import get_age_carbon_14_dating
 
 # Write a unit test which feed 0.35 to the function.
 # The result should be '8680.34'. Does the function handles
@@ -10,3 +9,15 @@ from ..carbon_dating import get_age_carbon_14_dating
 # Add the necessary logic to make sure the function handle
 # every possible input properly. Then write a unit test againt
 # this special case.
+
+
+def test_age_carbon_14_dating():
+    ratio = 0.35
+    exprected_result = 8680.34
+
+    if ratio <= 0:
+        with pytest.raises(TypeError):
+            get_age_carbon_14_dating(ratio)
+
+    assert math.isclose(get_age_carbon_14_dating(ratio),
+                        exprected_result, abs_tol=0.5)
